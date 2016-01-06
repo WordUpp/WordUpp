8b61cc8b33e4ef5967a441f043df4f8e6bb4f114
Ruth Thatcher <runic77@gmail.com>
2016-01-06 09:52:18 -0600

    adjusted and deleted route files

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,9 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/homepage', function(req, res, next) {
-  res.render('homepage', { title: 'Express' });
+  res.render('homepage', {'content-type': 'text/html'});
 });
 
+/* GET registration page. */
+router.get('/registration', function(req, res, next) {
+  res.render('registration', {'content-type': 'text/html'});
+});
+
+/* GET dictionary(search results) page. */
+router.get('/dictionary', function(req, res, next) {
+  res.render('dictionary', {'content-type': 'text/html'});
+});
 
 
 module.exports = router;

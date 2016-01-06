78ba596e8b5525a6c96a7cb43fc28680a255b2b0
Jen Kahn <jenniferhkahn@gmail.com>
2016-01-06 16:12:34 -0600

    passport stuff

--- models/user.js
+++ models/user.js
@@ -3,8 +3,14 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-  username: String,
-  password: String
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 User.plugin(passportLocalMongoose);

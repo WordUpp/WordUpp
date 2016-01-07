bbede0924a9a9ef4fbe2bf2c63157287f31bafd5
Ruth Thatcher <runic77@gmail.com>
2016-01-07 11:12:41 -0600

    fixed homepagejs

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var siteData = {
+  title: 'Wordupp API',
+  version: '1.0.0',
+  authors: 'WDI students',
+  date: 'Jan. 5, 2015'
+};
+
 router.get('/', function(req, res){
   res.render('layout', { 'layout': 'layout' })
-});
-
+})
 /* GET home page. */
 router.get('/homepage', function(req, res, next) {
-res.render('homepage', {'content-type': 'text/html'});
-});
 var data = siteData;
 data.user = req.user;
 res.render('homepage', data);
-
+res.render('homepage', {'content-type': 'text/html'});
+});
 
 
 module.exports = router;

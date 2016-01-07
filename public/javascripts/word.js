af38ae3f911143b4c39c4164e695428e02eeb3b9
Lamthong Keophalychanh <thea913@gmail.com>
2016-01-07 17:47:06 -0600

    we got words of the day!

--- public/javascripts/word.js
+++ public/javascripts/word.js
@@ -120,16 +120,20 @@
 // // make the ajax call
 // $.ajax(ajaxArgument);
 
-$(function (){
 
-  var $wod = $('#wod');
-  $.ajax({
-    type: 'GET',
-    url: '/word',
-    success: function(wod) {
-      $.each(wod, function(i, wod) {
-        $wod.append($('body'));
-      });
-    }
-  });
-});
+
+$.ajax({
+      type: 'GET',
+      url: '/word',
+      success: function(wod) {
+        // $.each(wod, function(i, wod) {
+        //   $wod.append($('body'));
+        // });
+        var index = Number.parseInt(Math.random()*wod.length)
+        $('body').append(wod[index].Word);
+        $('#type').append(wod[index].Word_type);
+        $('#sound').append(wod[index].Pronunciation);
+        $('#example').append(wod[index].Sentence);
+        $('#origin').append(wod[index].Origin);
+      }
+    });

b0b9cb9432f1c60b8286c9e49463d7fcea661a79
Jen Kahn <jenniferhkahn@gmail.com>
2016-01-07 09:47:55 -0600

    pushing up

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,27 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var siteData = {
-  title: 'Wordupp API',
-  version: '1.0.0',
-  authors: 'WDI students',
-  date: 'Jan. 5, 2015'
-};
-
 router.get('/', function(req, res){
   res.render('layout', { 'layout': 'layout' })
-})
+});
+
 /* GET home page. */
 router.get('/homepage', function(req, res, next) {
-var data = siteData;
-data.user = req.user;
-res.render('homepage', data);
 res.render('homepage', {'content-type': 'text/html'});
 });
+// var data = siteData;
+// data.user = req.user;
+// res.render('homepage', data);
 
 /* GET registration page. */
 router.get('/registration', function(req, res, next) {
-  res.render('registration', {'content-type': 'text/html'});
+res.render('registration', {'content-type': 'text/html'});
 });
 
 /* GET dictionary(search results) page. */

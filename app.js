422e8381124e56a31e4c3873a09f1c5ccafce6ab
Jennifer Kahn <jenniferkahn>
2016-02-16 09:26:37 -0600

    heroku fix

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
 //declare passport before db
 var mongoose = require('mongoose');
 var passport = require('passport');
